0d7d99d0df932d9dc39e2081cab0b5ce2f3ed3b5
TheLazySol <tbelonog25@gmail.com>
2024-05-18 16:47:34 -0400

    Created PDAs and updated rnd_anchor_framework to version 0.1.8

--- programs/rnd_anchor_framework/src/lib.rs
+++ programs/rnd_anchor_framework/src/lib.rs
@@ -1,15 +1,65 @@
 use anchor_lang::prelude::*;
 
-declare_id!("FZkMHQsM2ELE7baJvWJfZcL9cZFdWWNVPCqV2Fz1Eb7u");
+declare_id!("58ybx3ASqW2gvV8ejmiQfGKAMPrbBkjpipzfnQSN6vLh");
 
 #[program]
 pub mod rnd_anchor_framework {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_ledger(
+        ctx: Context<CreateLedger>, // context that is passed to the create_ledger function.
+        color: String,
+    ) -> Result<()> {
+        
+        let ledger_account = &mut ctx.accounts.ledger_account; // get the ledger account from the context.
+        ledger_account.color = color; // set the color of the ledger account.
+        ledger_account.balance = 0; // set the balance of the ledger account.
+
+        Ok(())
+    }
+
+    pub fn modify_ledger(
+        ctx: Context<ModifyLedger>,
+        new_balance: u32,
+    ) -> Result<()> {
+
+        let ledger_account = &mut ctx.accounts.ledger_account; // get the ledger account from the context.
+        ledger_account.balance = new_balance; // set the balance of the ledger account.
+
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[derive(Accounts)] // accounts that are passed to the create_ledger function.
+#[instruction(color: String)] // instruction that is passed to the create_ledger function. This part of the code needs to be able to know what "color" is when referencing it.
+pub struct CreateLedger<'info> {
+    #[account(
+        init,
+        payer = wallet, // this is the wallet that will pay for the creation of the PDA accounts.
+        space = 264, // this is the size of the data structure that will be stored in the PDA accounts.
+        seeds = [ // PDA account seeds are needed. Use this format to pass them: {wallet.publicKey+"_"+color} in this particular case.
+            wallet.key().as_ref(),
+            b"_",
+            color.as_ref(),
+        ], 
+        bump
+    )] 
+    pub ledger_account: Account<'info, Ledger>, // this is the account that will be created in the PDA accounts.
+    #[account(mut)]
+    pub wallet: Signer<'info>, // this is the wallet that will be used to sign the transaction.
+    pub system_program: Program<'info, System>, // this is the system program that will be used to create the PDA accounts.
+}
+
+#[derive(Accounts)] // accounts that are passed to the modify_ledger function.
+pub struct ModifyLedger<'info> {
+    #[account(mut)]
+    pub ledger_account: Account<'info, Ledger>, // this is the account that will be modified in the PDA accounts.
+    #[account(mut)]
+    pub wallet: Signer<'info>, // this is the wallet that will be used to sign the transaction.
+}
+
+#[account] // this is the data structure that will be stored in the PDA accounts aka houses the data in the PDA accounts.
+pub struct Ledger {
+    pub color: String,
+    pub balance: u32,
+}
--- tests/rnd_anchor_framework.ts
+++ tests/rnd_anchor_framework.ts
@@ -2,15 +2,82 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { RndAnchorFramework } from "../target/types/rnd_anchor_framework";
 
-describe("rnd_anchor_framework", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+function shortKey(key: anchor.web3.PublicKey) {
+  return key.toString().slice(0, 8);
+} // Shorten public key for easier logging.
 
+describe("rnd_anchor_framework", () => {
+  
+  const provider = anchor.AnchorProvider.env(); // Configure the provider.
+  anchor.setProvider(anchor.AnchorProvider.env()); // Configure the provider.
   const program = anchor.workspace.RndAnchorFramework as Program<RndAnchorFramework>;
 
+  async function generateKeyPair() {
+    const keypair = anchor.web3.Keypair.generate(); // Generate a new keypair.
+    await provider.connection.requestAirdrop(keypair.publicKey, 2e9); // Request airdrop for the keypair.
+    console.log("Generated keypair and airdropped 2 SOL", shortKey(keypair.publicKey));
+    await new Promise((resolve) => setTimeout(resolve, 3 * 1000)); // Wait for airdrop to complete.
+    return keypair;
+  }
+
+  async function derivePda(color: string, pubkey: anchor.web3.PublicKey) { // Derive a PDA.
+    let [pda, _] = await anchor.web3.PublicKey.findProgramAddressSync( // Derive the PDA.
+      [], // Seed.
+      program.programId // Program ID.
+    );
+    return pda; // Return the PDA. 
+  }
+
+  async function createLedgerAccount(
+    color: string, 
+    pda: anchor.web3.PublicKey,
+    wallet: anchor.web3.Keypair) {
+      await program.methods.createLedger(color)
+    } 
+
+  async function modifyLedger(
+    color: string,
+    newBalance: number,
+    wallet: anchor.web3.Keypair
+  ) { 
+    console.log("-------------------------------------------------------------------------");
+    let data;
+    let pda = await derivePda(color, wallet.publicKey);
+
+    console.log("Checking if account ${shortKey(pda)} exists for color ${color}...");
+    try {
+      console.log("It does exist!");
+      data = await program.account.ledger.fetch(pda);
+    } catch (e) {
+
+      console.log("It does not exist! Creating account...");
+      await createLedgerAccount(color, pda, wallet);
+      data = await program.account.ledger.fetch(pda);
+    };
+
+    console.log("Success.");
+    console.log("Data:");
+    console.log("Color: ${data.color} | Balance: ${data.balance}");
+    console.log("Modifying balance of ${data.color} from ${data.balance} to ${newBalance}...");
+
+    await program.methods.modifyLedger(newBalance)
+    .accounts({
+      ledgerAccount: pda,
+      wallet: wallet.publicKey,
+    })
+    .signers([wallet])
+    .rpc();
+  }
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+    const testKeypair1 = await generateKeyPair();
+    await modifyLedger("red", 2, testKeypair1);
+    await modifyLedger("red", 4, testKeypair1);
+    await modifyLedger("blue", 2, testKeypair1);
+
+    const testKeypair2 = await generateKeyPair();
+    await modifyLedger("red", 3, testKeypair2);
+    await modifyLedger("green", 3, testKeypair2);
   });
 });
